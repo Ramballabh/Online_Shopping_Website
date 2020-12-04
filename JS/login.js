c68fffdae777df23ff5b9fb7a41f4d6b8d74a9ba
Ramballabh Agrawal <ayush.goyal_cs18@gla.ac.in>
2020-12-04 16:45:03 +0530

    add content to login.js

---
+++ JS/login.js
@@ -0,0 +1,5 @@
+//Selector
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password1 = document.getElementById('password1');
+const small =  document.querySelector('small');
