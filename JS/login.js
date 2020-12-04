93174d261471f4f20eb40af242eade585abeb32a
Ramballabh Agrawal <ayush.goyal_cs18@gla.ac.in>
2020-12-04 16:47:45 +0530

    add function to check input

--- JS/login.js
+++ JS/login.js
@@ -10,3 +10,23 @@ form.addEventListener('submit',function(e){
     e.preventDefault();
     checkInput();
 });
+
+//Functions
+
+function checkInput(){
+    const usernameValue = username.value.trim();
+    const password1Value = password1.value.trim();
+    if(usernameValue === ''){
+        showError(username,"Username can not be blank");
+    }
+    else{
+        showSuccess(username);
+    }
+
+    if(password1Value === ''){
+        showError(password1,"Password can not be blank");
+    }
+    else{
+        showSuccess(password1);
+    }
+}
