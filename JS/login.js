8b4da4730bbba69ef061fde7a78a99b75313f3c2
Ramballabh Agrawal <ayush.goyal_cs18@gla.ac.in>
2020-12-04 16:46:18 +0530

    add event handler

--- JS/login.js
+++ JS/login.js
@@ -3,3 +3,10 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const password1 = document.getElementById('password1');
 const small =  document.querySelector('small');
+
+
+//Event Handler
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    checkInput();
+});
